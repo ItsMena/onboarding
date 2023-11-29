5c7ef9f49d91e8de992a8dece3d69d5e9aa2fb3b
MenaAbdelmalak <mea190@purple.rutgers.edu:29418/CourseOfSchedule.git>
2023-11-29 14:03:00 -0500

    login + minimal home page

--- onboarding/src/app/app-routing.module.ts
+++ onboarding/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationComponent } from './Screens/authentication/authentication.component';
+import { HomeComponent } from './Screens/home/home.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AuthenticationComponent},
+  {path: 'app', component: AppComponent},
+  {path: 'home', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- onboarding/src/app/app.module.ts
+++ onboarding/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Screens/home/home.component';
+import { FormComponent } from './Screens/form/form.component';
+import { AuthenticationComponent } from './/Screens/authentication/authentication.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FormComponent,
+    AuthenticationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
